4d98c17fafcb97dcec4f8d5589e75e8ebb97c8f2
hjfritz <fritzhj@gmail.com>
2022-09-06 22:01:51 -0700

    Improved Inventory

--- The Gods' Rube Goldberg/Assets/Scripts/HeavenInventoryManager.cs
+++ The Gods' Rube Goldberg/Assets/Scripts/HeavenInventoryManager.cs
@@ -1,9 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HeavenInventoryManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _waterSpoutInvPrefab;
+    [SerializeField] private GameObject _railsInvPrefab;
+    [SerializeField] private GameObject _eIHeadInvPrefab;
+    
+    [SerializeField] private Transform _waterSpoutInvPosition;
+    [SerializeField] private Transform _railsInvPosition;
+    [SerializeField] private Transform _eIHeadInvPosition;
+    
+    [SerializeField] private GameObject _waterSpoutPrefab;
+    [SerializeField] private GameObject _railsPrefab;
+    [SerializeField] private GameObject _eIHeadPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +27,40 @@ public class HeavenInventoryManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("head") || CompareTag("rails") || CompareTag("spout"))
+        {
+            //other.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("head"))
+        {
+            var pos = other.transform;
+            other.gameObject.SetActive(false);
+            Instantiate(_eIHeadPrefab, pos);
+            Instantiate(_eIHeadInvPrefab, _eIHeadInvPosition);
+        }
+        
+        if (other.CompareTag("rails"))
+        {
+            var pos = other.transform;
+            other.gameObject.SetActive(false);
+            Instantiate(_railsPrefab, pos);
+            Instantiate(_railsInvPrefab, _railsInvPosition);
+        }
+        
+        if (other.CompareTag("spout"))
+        {
+            var pos = other.transform;
+            other.gameObject.SetActive(false);
+            Instantiate(_waterSpoutPrefab, pos);
+            Instantiate(_waterSpoutInvPrefab, _waterSpoutInvPosition);
+        }
     }
 }
